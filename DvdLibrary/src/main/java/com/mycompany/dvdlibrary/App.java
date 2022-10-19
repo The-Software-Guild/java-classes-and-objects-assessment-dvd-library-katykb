66be86e0c4704f55a22d98bc4203dc6214bfa947
Katy Brown <katybr35@gmail.com>
2022-10-19 13:16:04 -0500

    created program shell

---
+++ DvdLibrary/src/main/java/com/mycompany/dvdlibrary/App.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.dvdlibrary;
+
+/**
+ * Allow the user to add a DVD to the collection Allow the user to remove a DVD
+ * from the collection Allow the user to edit the information for an existing
+ * DVD in the collection Allow the user to list the DVDs in the collection Allow
+ * the user to display the information for a particular DVD Allow the user to
+ * search for a DVD by title Load the DVD library from a file Save the DVD
+ * library back to the file when the program completes Allow the user to add,
+ * edit, or delete many DVDs in one session
+ *
+ * Additionally, the program must follow the MVC design pattern and use
+ * dependency injection as shown in the course material.
+ *
+ * You should follow the process outlined in the Agile Approach Checklist for
+ * Console Applications document elsewhere in this course.
+ *
+ * Your DVD data transfer object should have the following fields: This is all
+ * your input: Title Release date MPAA rating Director's name Studio User rating
+ * or note (allows the user to enter additional information, e.g., "Good family
+ * movie")
+ *
+ * @author katyb
+ */
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        int numChoice = mainMenu();
+        System.out.println("Choice: " + numChoice);
+
+    }
+
+    public static int mainMenu() {
+        //Create a menu for the user:
+        System.out.println("DVD Libray");
+        System.out.println("");
+        System.out.println("Main Menu");
+        System.out.println("1 - List Collection");
+        System.out.println("2 - List one Title");
+        System.out.println("3 - Add DVD");
+        System.out.println("4 - Delete DVD");
+        System.out.println("5 - Edit DVD");
+        System.out.println("6 - Search Library");
+        System.out.println("7 - Exit\n\n");
+        System.out.println("Please choose an option: ");
+
+        Scanner userChoice = new Scanner(System.in);
+        int numChoice = userChoice.nextInt();
+
+        return numChoice;
+
+    }
+}
---
+++ DvdLibrary/src/main/java/com/sal/dvdlibrary/dao/dvdLibraryDao.java
@@ -0,0 +1,14 @@
+package com.sal.dvdlibrary.dao;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author katyb
+ */
+public class dvdLibraryDao {
+    
+}
